19aa881c18c502b43d0868c0cdb9b16d952cf396
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-05 09:47:00 +0200

    50 neue Vokabeln pro Kategorie ergänzt

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelen-v5';
+const CACHE = 'vokabelen-v6';
 const ASSETS = ['/', '/static/icon-192.png', '/static/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
