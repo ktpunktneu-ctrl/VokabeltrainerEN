09ea404cb76d5444e6185445837369d8c0fb578a
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-05 12:21:49 +0200

    Fix: relative Pfade fuer manifest/icon/sw.js (GitHub Pages Subpath-Bug)

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelen-v6';
+const CACHE = 'vokabelen-v7';
 const ASSETS = ['/', '/static/icon-192.png', '/static/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
