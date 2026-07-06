b5f4a0eb240e98bc574ff4af20f79349909cfb4b
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-06 08:09:17 +0200

    Suchfunktion in Lernliste und Verwaltung ergaenzt (eigenstaendig, hat Vorrang vor Kategorie-Filter)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelen-v10';
+const CACHE = 'vokabelen-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './static/icon-192.png', './static/icon-512.png'];
 
 self.addEventListener('install', e => {
