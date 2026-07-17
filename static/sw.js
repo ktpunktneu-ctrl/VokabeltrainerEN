e927b1e9eba3b9424b1609a7c96c3acdcd2f58f8
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-17 08:22:40 +0200

    Sprachbezeichnung 'Englisch' im Header ergänzt (analog BusinessEN), Cache-Version erhöht

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelen-v18';
+const CACHE = 'vokabelen-v19';
 const ASSETS = ['/', '/static/icon-192.png', '/static/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
